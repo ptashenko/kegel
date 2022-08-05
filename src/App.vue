60a8c38ae3123ee745d430ecdf54aea860be2be9
Анатолий Сельденков <talyanze@gmail.com>
2022-08-05 18:26:12 +0300

    Сохранение прогреса при обновлении

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,15 @@ export default {
   methods: {
     ...mapActions(['getContent', 'getTrack', 'getHistory', 'getSeparators', 'generatUUID']),
   },
-  computed: {
+  created () {
+    // Считываем информацию о статусе в sessionStorage при загрузке страницы
+    if (sessionStorage.getItem("store") ) {
+        this.$store.replaceState(Object.assign({}, this.$store.state,JSON.parse(sessionStorage.getItem("store"))))
+    } 
+    // Сохраняем информацию в vuex в sessionStorage при обновлении страницы
+    window.addEventListener("beforeunload",()=>{
+        sessionStorage.setItem("store",JSON.stringify(this.$store.state))
+    })
   },
   watch:{
   },

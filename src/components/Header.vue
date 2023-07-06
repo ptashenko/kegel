0f393e6d35c5c9580cd4284aa9847a6b8b524f68
Artem Ptashenko <aptashenko2019@gmail.com>
2023-07-06 20:46:04 +0300

    [KQ-23] fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,6 @@
 <template>
   <header class="relative top-0 left-0 right-0 pt-24px pb-32px mx-auto bg-transparent max-w-311px sm:max-w-520px sm:py-32px" :class="{'!absolute': $route.name === 'home' }">
-    <router-link to="/" class="">
-      <img :src="require(`@/assets/images/logos/${logo}.png`)" alt="logo" class="inline-block w-120px sm:w-150px">
-    </router-link>
+    <img :src="require(`@/assets/images/logos/${logo}.png`)" alt="logo" class="inline-block w-120px sm:w-150px">
   </header>
 </template>
 
